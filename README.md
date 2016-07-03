f8eb8078e1d07d788a1329ab35fb2828bcbea331
Ian <ianmobbs@gmail.com>
2016-07-03 03:54:05 -0500

    Mostly functional. Everything works except song/artist/album don't always update and hyperlinks don't work

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Listen to the KVRX Livestream without having to keep a tab open.
 ##Status
 Functional. Need to add song/artist/album/album artwork display support.
 ##TODO
+* Biggest TODO: Consistently update "now playing"
+* Remove (or fix) hyperlinks from albums
 * Match the KVRX design scheme
 * Use switch in background.js
--- popup.js
+++ popup.js
@@ -1,9 +1,16 @@
 //Updates song, artist, album, and album art tags from KVRX website
 function updateTags() {
+	//console.log("updated")
 	$("#song").load("http://kvrx.org #track-name:eq(0)");
 	$("#artist").load("http://kvrx.org #artist-name:eq(0)");
 	$("#album").load("http://kvrx.org #album-name:eq(0)");
 	$("#albumart").load("http://kvrx.org #album-artwork");
+	/*
+	var strSongName = $('#song').contents().unwrap().contents().unwrap().text();
+	var strArtistName = $('#artist').contents().unwrap().contents().unwrap().text();
+	var strAlbumName = $('#album').contents().unwrap().contents().unwrap().text();
+	console.log("Info:", strSongName, strArtistName, strAlbumName);
+	*/
 }
 
 //Runs scripts once main page is loaded
@@ -14,17 +21,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	//Create variables for play and pause buttons
     var playButton = document.getElementById('livestreamlisten'),
-    	stopButton = document.getElementById('livestreamstop')
+    	stopButton = document.getElementById('livestreamstop');
     
    	//If play button is pressed, send to background.js via message
     playButton.addEventListener('click', function() {
-    	console.log("play button pressed");
+    	//console.log("play button pressed");
     	chrome.extension.sendMessage({action: "play"})
     });
     
     //If pause button is pressed, send to background.js via message
     stopButton.addEventListener('click', function () {
-    	console.log("stop button pressed");
+    	//console.log("stop button pressed");
     	chrome.extension.sendMessage({action: "pause"})
     })
 
