ad1400b3fc3e0866e53e7976feb5d77045bfb33e
Ian <ianmobbs@gmail.com>
2016-07-02 20:20:30 -0500

    Fixed double instances of meta song information, commented everything

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//Creates audio element
 var audioElement = document.createElement('audio');
 audioElement.setAttribute("preload", "auto");
 audioElement.autobuffer = true;
@@ -7,11 +8,14 @@ source1.type = 'audio/mpeg';
 source1.src = 'http://tstv-stream.tsm.utexas.edu:8000/kvrx_livestream';
 audioElement.appendChild(source1);
 
+//Listens for messages
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
+        //Plays audio element if message is play
         if (request.action == "play"){
             audioElement.load;
             audioElement.play();
+        //Pauses audio element if message is pause
         } else if (request.action == "pause") {
             audioElement.pause();
         }
--- popup.js
+++ popup.js
@@ -1,25 +1,28 @@
+//Updates song, artist, album, and album art tags from KVRX website
 function updateTags() {
-	$("#song").load("http:/kvrx.org #track-name");
-	$("#artist").load("http:/kvrx.org #artist-name");
-	$("#album").load("http:/kvrx.org #album-name");
-	$("#albumart").load("http:/kvrx.org #album-artwork");
+	$("#song").load("http://kvrx.org #track-name:eq(0)");
+	$("#artist").load("http://kvrx.org #artist-name:eq(0)");
+	$("#album").load("http://kvrx.org #album-name:eq(0)");
+	$("#albumart").load("http://kvrx.org #album-artwork");
 }
 
+//Runs scripts once main page is loaded
 document.addEventListener('DOMContentLoaded', function() {
-
+	//Update tags and proceed to do it every five seconds
 	updateTags()
-	//setInterval(updateTags, 1000)
+	setInterval(updateTags, 5000)
 
-
+	//Create variables for play and pause buttons
     var playButton = document.getElementById('livestreamlisten'),
     	stopButton = document.getElementById('livestreamstop')
     
-
+   	//If play button is pressed, send to background.js via message
     playButton.addEventListener('click', function() {
     	console.log("play button pressed");
     	chrome.extension.sendMessage({action: "play"})
     });
     
+    //If pause button is pressed, send to background.js via message
     stopButton.addEventListener('click', function () {
     	console.log("stop button pressed");
     	chrome.extension.sendMessage({action: "pause"})
