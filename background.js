f69b868427c179fa8428943c04723ae5fdd26505
Ian Mobbs <ianmobbs@gmail.com>
2016-09-16 11:39:34 -0500

    Cleaned out the trash

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 //Class for KVRX player
 class KVRXPlayer {
 	constructor() {
-		//Set scrobbling to false to prevent accidental scrobbles when not listening
-		this.scrobbling = false;
-
 		//Create player audio element
 		console.log("Creating player element");
 		this.audioElement = document.createElement('audio');
@@ -22,120 +19,30 @@ class KVRXPlayer {
 		this.audioElement.load;
 	}
 	play() {
-		//Set scrobbling to true so we can scrobble tracks played.
-		console.log("Playing the tunes");
-		this.scrobbling = true;
-		
-		//Play the audio element, check to scrobble every 16 seconds
+		console.log("Play message received");
 		this.audioElement.play();
-		setInterval(checkScrobble, 16000)
 	}
 	pause() {
-		//Turn off scrobbling to prevent accidental scrobbles, pause the audio
-		console.log("Pausing the tunes");
-		this.scrobbling = false;
+		console.log("Stop message received");
 		this.audioElement.pause();
 	}
 }
-
-//Class Track holds a track defined by it's artist, song, and album
-class Track {
-	//Creates track with artist, song, and album
-	constructor(artist, title, album) {
-		this.artist = artist;
-		this.song = title;
-		this.album = album;
-		//Time created is automatically populated with current time
-		//Each track is not scrobbled by default
-		this.timeCreated = new Date().getTime();
-		this.scrobbled = false;
-	}
-	//Accessors - Self explanatory
-	getSong() {
-		return (this.song);
-	}
-	getArtist() {
-		return (this.artist);
-	}
-	getAlbum() {
-		return (this.album);
-	}
-	//Returns time in seconds since the time passed through to method
-	getTimeListened(time) {
-		return ((time - this.timeCreated) / 1000);
-	}
-	getScrobbled() {
-		return (this.scrobbled);
-	}
-	//Changes track status to "scrobbled"
-	//Log that track was scrobbled
-	scrobbleTrack() {
-		console.log("Scrobbled", this.getSong(), "-", this.getArtist())
-		this.scrobbled = true;
-	}
-	//Methods
-	//Checks to see if track, artist, and album are the same 
-	equal(t2) {
-		return (this.getSong() == t2.getSong() && this.getArtist() == t2.getArtist() && this.getAlbum == t2.getAlbum())
-	}
-}	
-
 //Create new instance of the KVRX player
 console.log("Instantiating new player class");
 var player = new KVRXPlayer();
 
-//Strips HTML tags from a string
-function strip(html) {
-   var tmp = document.createElement("DIV");
-   tmp.innerHTML = html;
-   return tmp.textContent || tmp.innerText || "";
-}
-
-//Scrobbles a track
-function scrobble(track) {
-	track.scrobbleTrack();
-	//Check if logged in
-	//Send to last.fm
-}
-
-//Store current track played to prevent multiple scrobbles
-var currTrack = new Track(null, null, null);
-//Scrobbles track being played
-function checkScrobble() {
-	//Gets song name and artist of track playing
-	var tempTrack;
-    $.get("http://kvrx.org/index.php", function(data) {
-        var song = $(data).find("[id=track-name]")[0].innerText;
-        	artist = $(data).find("[id=artist-name]")[0].innerText;
-        	album = $(data).find("[id=album-name]")[0].innerText;
-        	tempTrack = new Track(artist, song, album); //Possible err: scope of tempTrack
-
-    	if (!tempTrack.equal(currTrack)) {
-    		console.log("New track: ", tempTrack.song, " - ", tempTrack.artist)
-    		currTrack = tempTrack;
-    	} else {
-    		var timeListened = currTrack.getTimeListened(new Date().getTime());
-    		console.log("Listened to ", currTrack.getSong(), "for", timeListened, "seconds");
-    		if (timeListened >= 30 && !currTrack.getScrobbled()) {
-    			scrobble(currTrack);
-    		}
-    	}
-    });
-}
-
 //Listens for messages
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
+        
         //Plays audio element if message is play
         if (request.action == "play"){
         	console.log("Play button pressed");
             player.play();
+
         //Pauses audio element if message is pause
         } else if (request.action == "pause") {
         	console.log("Pause button pressed");
             player.pause();
-        } else if (request.action == "scrobble") {
-        	console.log("Scrobble received");
-        	scrobbleCheck(request.song, request.artist);
         }
 });
--- popup.js
+++ popup.js
@@ -15,13 +15,6 @@ function updateTags() {
         document.getElementById('album').innerText = $(data).find("[id=album-name]")[0].innerText;
         document.getElementById('albumart').innerHTML = $(data).find("[id=album-artwork]")[0].innerHTML;
         }); 
-    var currDay = new Date().getDay(),
-        currHours = new Date().getHours();
-        if (((currDay >= 1 && currDay <= 5) && (currHours >= 19 || currHours <= 9)) || (currDay == 0 || currDay == 6) && (currHours >= 22 && currHours <= 9)) { //Monday through Friday, 7pm to 9am, Saturday/Sunday, 10pm to 9am
-            var schedule = $.get("http://kvrx.org/schedule", function(data) {
-                
-            });
-        }
 }
 
 //Runs scripts once main page is loaded
@@ -31,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	//Create variables for play and pause buttons
     var playButton = document.getElementById('livestreamlisten'),
-    	stopButton = document.getElementById('livestreamstop');
+        stopButton = document.getElementById('livestreamstop');
     
    	//If play button is pressed, send to background.js via message
     playButton.addEventListener('click', function() {
@@ -40,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function() {
     
     //If pause button is pressed, send to background.js via message
     stopButton.addEventListener('click', function () {
-    	//console.log("stop button pressed");
     	chrome.extension.sendMessage({action: "pause"})
     })
 
